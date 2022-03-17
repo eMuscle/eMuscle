6c4f4505e415a57de310d36334d7d2f031cdd775
Tuomas Riihiaho <78533799+c0ritu00@users.noreply.github.com>
2022-03-17 12:48:28 +0200

    Added intent to login button % Corrected image

--- app/src/main/java/com/example/emuscle/LoginScreen.kt
+++ app/src/main/java/com/example/emuscle/LoginScreen.kt
@@ -1,18 +1,24 @@
 package com.example.emuscle
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+
+        val loginButton = findViewById<Button>(R.id.buLogin)
+
+        loginButton.setOnClickListener{
+            val intent = Intent(this, MainMenu::class.java)
+            startActivity(intent)
+        }
     }
 
-    fun login() {
-        val intent = Intent(this, MainMenu::class.java)
-        startActivity(intent)
-    }
+
+
 }
