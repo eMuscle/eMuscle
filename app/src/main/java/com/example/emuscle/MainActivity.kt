bd85534f86d329c88b2c841ddb8960b97099d086
c0latu02 <78533839+c0latu02@users.noreply.github.com>
2022-03-17 12:17:53 +0200

    added app intro timer

--- app/src/main/java/com/example/emuscle/MainActivity.kt
+++ app/src/main/java/com/example/emuscle/MainActivity.kt
@@ -3,18 +3,23 @@ package com.example.emuscle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        handler = Handler()
+        handler.postDelayed({
+
+            val intent = Intent(this, LoginScreen::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
 
     }
-  //  fun timer() {
-    //    val intent = Intent(this, LoginScreen::class.java)
-      //  startActivity(intent)
-   // }
-
 }
