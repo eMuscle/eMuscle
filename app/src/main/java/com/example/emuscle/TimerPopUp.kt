76499ff869a5ad685f8d36a234379eabcacc1c85
Tuomas Riihiaho <78533799+c0ritu00@users.noreply.github.com>
2022-04-01 16:52:53 +0300

    Added transparent popup to calendarDay and added some functionalities to it. UI also updated

--- app/src/main/java/com/example/emuscle/TimerPopUp.kt
+++ app/src/main/java/com/example/emuscle/TimerPopUp.kt
@@ -1,6 +1,8 @@
 package com.example.emuscle
 
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
 import android.app.Activity
@@ -10,6 +12,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import android.view.animation.DecelerateInterpolator
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -21,47 +24,44 @@ class TimerPopUp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         overridePendingTransition(0, 0)
         setContentView(R.layout.activity_timer_pop_up)
+        val popUpBackButton = findViewById<Button>(R.id.popup_window_button)
 
-        val ConstraintId = findViewById<ConstraintLayout>(R.id.popup_window_background)
-        val CardviewId = findViewById<CardView>(R.id.popup_window_view_with_border)
-
-        // Set the Status bar appearance for different API levels
-        if (Build.VERSION.SDK_INT in 19..20) {
-            setWindowFlag(this, true)
-        }
-        if (Build.VERSION.SDK_INT >= 19) {
-            window.decorView.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        }
-        if (Build.VERSION.SDK_INT >= 21) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-                this.window.statusBarColor = Color.TRANSPARENT
-                setWindowFlag(this, false)
-            }
+        // Close the Popup Window when you press the button
+        popUpBackButton.setOnClickListener {
+            onBackPressed()
         }
 
-        // Fade animation for the background of Popup Window
-        val alpha = 100 //between 0-255
+    }
+
+    //Back Button animation and activity close.
+    override fun onBackPressed() {
+        val popupWindowBackground = findViewById<ConstraintLayout>(R.id.popup_window_background)
+        val popupWindowBorder = findViewById<CardView>(R.id.popup_window_view_with_border)
+        // Fade animation for the background of Popup Window when you press the back button
+        val alpha = 100 // between 0-255
         val alphaColor = ColorUtils.setAlphaComponent(Color.parseColor("#000000"), alpha)
-        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), Color.TRANSPARENT, alphaColor)
+        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), alphaColor, Color.TRANSPARENT)
         colorAnimation.duration = 500 // milliseconds
         colorAnimation.addUpdateListener { animator ->
-            ConstraintId.setBackgroundColor(animator.animatedValue as Int)
+            popupWindowBackground.setBackgroundColor(
+                animator.animatedValue as Int
+            )
         }
+
+        // Fade animation for the Popup Window when you press the back button
+        popupWindowBorder.animate().alpha(0f).setDuration(500).setInterpolator(
+            DecelerateInterpolator()
+        ).start()
+
+        // After animation finish, close the Activity
+        colorAnimation.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                finish()
+                overridePendingTransition(0, 0)
+            }
+        })
         colorAnimation.start()
-
-
     }
 
-    private fun setWindowFlag(activity: Activity, on: Boolean) {
-        val win = activity.window
-        val winParams = win.attributes
-        if (on) {
-            winParams.flags = winParams.flags or WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-        } else {
-            winParams.flags = winParams.flags and WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS.inv()
-        }
-        win.attributes = winParams
-    }
+
 }
