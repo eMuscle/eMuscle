fa373e22ddc638f8a6750318225e155c1dcc6dc1
Tuomas Riihiaho <78533799+c0ritu00@users.noreply.github.com>
2022-04-06 14:34:54 +0300

    Exerciseview

--- app/src/main/java/com/example/emuscle/CalendarDay.kt
+++ app/src/main/java/com/example/emuscle/CalendarDay.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 
 class CalendarDay : AppCompatActivity() {
@@ -13,14 +14,22 @@ class CalendarDay : AppCompatActivity() {
 
         val id = intent.getStringExtra("id")
 
+        val addExerciseButton = findViewById<Button>(R.id.buExercise)
+        val exerciseLayout = findViewById<LinearLayout>(R.id.exercise)
         val dateText = findViewById<TextView>(R.id.date)
+
         dateText.text = id
+
+        addExerciseButton.setOnClickListener{
+            val exerciseView = layoutInflater.inflate(R.layout.exercise_row, null)
+            exerciseLayout.addView(exerciseView)
+        }
 
         val popup = findViewById<Button>(R.id.timerbutton)
         popup.setOnClickListener {
             val intent = Intent(this, TimerPopUp::class.java)
-
             startActivity(intent)
         }
+
     }
 }
