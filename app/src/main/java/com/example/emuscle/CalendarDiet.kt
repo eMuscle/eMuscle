c01dce75c8f0d77cd581a0c9a1c05d9346524a79
c0latu02 <78533839+c0latu02@users.noreply.github.com>
2022-04-13 10:29:22 +0300

    small changes

--- app/src/main/java/com/example/emuscle/CalendarDiet.kt
+++ app/src/main/java/com/example/emuscle/CalendarDiet.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CalendarView
-import kotlinx.android.synthetic.main.activity_calendar_diet.*
+//import kotlinx.android.synthetic.main.activity_calendar_diet.*
 
 class CalendarDiet : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
