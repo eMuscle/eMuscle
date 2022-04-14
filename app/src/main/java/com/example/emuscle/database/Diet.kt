4ebfe398e323c57fdfa93f941606e10e9ea079ee
c0latu02 <78533839+c0latu02@users.noreply.github.com>
2022-04-14 10:47:44 +0300

    added db

---
+++ app/src/main/java/com/example/emuscle/database/Diet.kt
@@ -0,0 +1,25 @@
+package com.example.emuscle.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "diet_table")
+
+data class Diet(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val day: String,
+    val breakFast: String,
+    val breakFastCal: Int,
+    val lunch: String,
+    val lunchCal: Int,
+    val dinner: String,
+    val dinnerCal: Int,
+    val supper: String,
+    val supperCal: Int,
+    val snacks: String,
+    val SnacksCal: Int
+)
+
+
+
